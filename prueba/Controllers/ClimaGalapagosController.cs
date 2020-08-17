418ba3add861aecf7b590324a63e07c5ce2d58f0
Anthony Vinicio <avs.larrea@yavirac.edu.ec>
2020-08-16 20:43:51 -0500

    prototipos

---
+++ prueba/Controllers/ClimaGalapagosController.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+namespace ZooLine.Views.ClimaGalapagos
+{
+    public class ClimaGalapagosController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
