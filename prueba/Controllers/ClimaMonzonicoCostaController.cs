d89d12eb808e3cede46b016ffdbd47840c462f38
Anthony Vinicio <avs.larrea@yavirac.edu.ec>
2020-08-17 22:53:06 -0500

    climasRegiones

--- prueba/Controllers/ClimaMonzonicoCostaController.cs
+++ prueba/Controllers/ClimaMonzonicoCostaController.cs
@@ -4,9 +4,9 @@ using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
 
-namespace ZooLine.Views.Costa
+namespace ZooLine.Views.ClimaMonzonicoCosta
 {
-    public class CostaController : Controller
+    public class ClimaMonzonicoCostaController : Controller
     {
         public IActionResult Index()
         {
---
+++ prueba/Controllers/ClimaTropicalSierraController.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+namespace ZooLine.Views.ClimaTropicalSierra
+{
+    public class ClimaTropicalSierraController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
