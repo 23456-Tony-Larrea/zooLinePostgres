8f057fe89aba234ff21c2e6b6d7cbf56dbb0f506
Anthony Vinicio <avs.larrea@outlook.com>
2020-09-03 00:15:42 -0500

    SelectRegionesHabitats

--- prueba/Controllers/ClimaCalidoCostaController.cs
+++ prueba/Controllers/ClimaCalidoCostaController.cs
@@ -17,19 +17,19 @@ namespace ZooLine.Views.Costa
         [Route("[controller]/{id?}")]
         public async  Task<IActionResult> Index(string id)
         {
-            if (!int.TryParse(id, out var EspecieId))
-                return View(new List<CardModel>());
+                if (!int.TryParse(id, out var EspecieId))
+                    return View(new List<CardModel>());
 
-           var animales =  await _dbAplicacion.Animales.Where(x=> x.EspecieId == EspecieId).OrderByDescending(x => x.AnimalId).Select(x => new CardModel
-            {
-                Descripcion = x.descripcion,
-                SubDescripcion = x.año_muerte.ToString(),
-                ImageUrl = x.NombreImagen,
-                SubTitulo = x.NombreCientifico,
-                Titulo = x.Nombre
+               var animales =  await _dbAplicacion.Animales.Where(x=> x.EspecieId == EspecieId).OrderByDescending(x => x.AnimalId).Select(x => new CardModel
+                {
+                    Descripcion = x.descripcion,
+                    SubDescripcion = x.año_muerte.ToString(),
+                    ImageUrl = x.NombreImagen,
+                    SubTitulo = x.NombreCientifico,
+                    Titulo = x.Nombre
 
-            }).ToListAsync();
-            return View(animales);
+                }).ToListAsync();
+                return View(animales);
 
         }
     
--- prueba/Controllers/ClimaPantanosoOrienteController.cs
+++ prueba/Controllers/ClimaPantanosoOrienteController.cs
@@ -3,14 +3,37 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using prueba.Data;
+using ZooLine.Models;
 
 namespace ZooLine.Views.ClimaPantanosoOriente
 {
     public class ClimaPantanosoOrienteController : Controller
     {
-        public IActionResult Index()
+        private readonly ApplicationDbContext _dbAplicacion;
+        public ClimaPantanosoOrienteController(ApplicationDbContext context)
         {
-            return View();
+            _dbAplicacion = context;
         }
+        [Route("[controller]/{id?}")]
+        public async Task<IActionResult> Index(string id)
+        {
+            if (!int.TryParse(id, out var EspecieId))
+                return View(new List<CardModel>());
+
+            var animales = await _dbAplicacion.Animales.Where(x => x.EspecieId == EspecieId).OrderByDescending(x => x.AnimalId).Select(x => new CardModel
+            {
+                Descripcion = x.descripcion,
+                SubDescripcion = x.año_muerte.ToString(),
+                ImageUrl = x.NombreImagen,
+                SubTitulo = x.NombreCientifico,
+                Titulo = x.Nombre
+
+            }).ToListAsync();
+            return View(animales);
+
+        }
+
     }
 }
